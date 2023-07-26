311ff2ce6d468491c5521236e4c943910f8c92f9
Girish <skillqode.gk@gmail.com>
2023-07-26 10:04:24 +0530

    26/7/2023

---
+++ incredecrment.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int a=10;
+    printf("a:%d,a:%d",a,a++);
+    //printf("a:%d,a:%d",a++,++a);
+    //printf("a:%d,a:%d",a,++a);
+    //printf("a:%d,a:%d",a++,++a);
+    //printf("a:%d,a:%d,a:%d",a,a++,++a);
+    //printf("a:%d,a:%d,a:%d",++a,a++,++a);
+}
